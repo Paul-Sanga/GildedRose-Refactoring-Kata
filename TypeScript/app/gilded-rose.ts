a3080f7d0b817705173a30d53fe1ec5c5be476a0
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-10-23 10:21:58 +0300

    Challenge

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -20,23 +20,25 @@ export class GildedRose{
   updateQuality(): Array<Item>{
 
       for(let i:number = 0; i<this.items.length; i++){
-          if(this.items[i].name == 'Aged Brie'  && this.items[i].quality < 50){
+          if(this.items[i].name == 'Aged Brie' && this.items[i].quality < 50){
               this.items[i].quality += 1;
               this.items[i].sellIn -= 1;
+          }else if(this.items[i].name == 'Aged Brie' && this.items[i].quality == 50){
+            this.items[i].sellIn -= 1;
           }else if(this.items[i].name == 'Sulfuras'){
               this.items[i].quality = this.items[i].quality == 80 ? this.items[i].quality : 80;
               this.items[i].sellIn -= 1;
           }else if(this.items[i].name == 'Conjured' && this.items[i].quality > 0){
-              this.items[i].quality = (this.items[i].quality -=2) >= 0 ? this.items[i].quality -=2 : 0;
+              this.items[i].quality = (this.items[i].quality -=2) >= 0 ? 0 : this.items[i].quality -=2;
               this.items[i].sellIn -= 1;
           }else if(this.items[i].name == 'Backstage passes' && this.items[i].sellIn < 0){
-              this.items[i].quality = (this.items[i].quality-=2) > 0 ? this.items[i].quality : 0;
+              this.items[i].quality = 0;
               this.items[i].sellIn -= 1;
           }else if(this.items[i].name == 'Backstage passes' && this.items[i].sellIn <= 5){
-              this.items[i].quality += 3;
+              this.items[i].quality = ((this.items[i].quality +=3) > 50) ? 50 : this.items[i].quality +=3;
               this.items[i].sellIn -= 1;
           }else if(this.items[i].name == 'Backstage passes' &&  this.items[i].sellIn <= 10){
-              this.items[i].quality += 2;
+              this.items[i].quality = ((this.items[i].quality +=2) > 50) ? 50 : this.items[i].quality +=2;;
               this.items[i].sellIn -= 1;
           }else{
               this.items[i].quality -= 1;
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,23 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+
+  it('should increase quality of Aged Brie product', ()=>{
+    let item: Item[] = [new Item('Aged Brie', 30, 39)]
+    let guildedRose: GildedRose = new GildedRose(item)
+    guildedRose.updateQuality()
+    expect(guildedRose.items[0].sellIn).toEqual(29)
+    expect(guildedRose.items[0].quality).toBe(40)
+  })
+
+  it('should maintain quality of Aged Brie product if quality is at 50', ()=>{
+    let item: Item[] = [new Item('Aged Brie', 30, 50)]
+    let guildedRose: GildedRose = new GildedRose(item)
+    guildedRose.updateQuality()
+    expect(guildedRose.items[0].sellIn).toEqual(29)
+    expect(guildedRose.items[0].quality).toEqual(50)
+  })
+
+  it('should maintain quality of')
+
 });
